79eed80e710ca37a6dba35f338be72b5da1769ff
D4nf313 <danfecisf@gmail.com>
2025-04-28 18:57:05 -0500

    maqueta 1 login

--- src/app/login/login/login.component.ts
+++ src/app/login/login/login.component.ts
@@ -1,8 +1,13 @@
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { NzButtonModule } from 'ng-zorro-antd/button';
+import { NzCardModule } from 'ng-zorro-antd/card';
+import { NzFormModule } from 'ng-zorro-antd/form';
+import { NzInputModule } from 'ng-zorro-antd/input';
 
 @Component({
   selector: 'app-login',
-  imports: [],
+  imports: [FormsModule, NzFormModule, NzInputModule, NzButtonModule, NzCardModule],
   templateUrl: './login.component.html',
   styleUrl: './login.component.css'
 })
