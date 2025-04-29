e67e637388f1fdc1ddc6712c7a391bbeb48fbd13
D4nf313 <danfecisf@gmail.com>
2025-04-28 23:24:51 -0500

    add recaptcha

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,4 +1,8 @@
-import { ApplicationConfig, provideZoneChangeDetection, importProvidersFrom } from '@angular/core';
+import {
+  ApplicationConfig,
+  provideZoneChangeDetection,
+  importProvidersFrom,
+} from '@angular/core';
 import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
@@ -8,9 +12,17 @@ import en from '@angular/common/locales/en';
 import { FormsModule } from '@angular/forms';
 import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
 import { provideHttpClient } from '@angular/common/http';
+import { RecaptchaModule } from 'ng-recaptcha';
 
 registerLocaleData(en);
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideZoneChangeDetection({ eventCoalescing: true }), provideRouter(routes), provideNzI18n(en_US), importProvidersFrom(FormsModule), provideAnimationsAsync(), provideHttpClient()]
+  providers: [
+    provideZoneChangeDetection({ eventCoalescing: true }),
+    provideRouter(routes),
+    provideNzI18n(en_US),
+    importProvidersFrom(FormsModule, RecaptchaModule),
+    provideAnimationsAsync(),
+    provideHttpClient(),
+  ],
 };
--- src/app/login/login/login.component.ts
+++ src/app/login/login/login.component.ts
@@ -1,5 +1,6 @@
-import { Component } from '@angular/core';
-import { FormsModule } from '@angular/forms';
+import { Component, inject } from '@angular/core';
+import { FormBuilder, FormGroup, FormsModule, Validators } from '@angular/forms';
+import { RecaptchaModule } from 'ng-recaptcha';
 import { NzButtonModule } from 'ng-zorro-antd/button';
 import { NzCardModule } from 'ng-zorro-antd/card';
 import { NzFormModule } from 'ng-zorro-antd/form';
@@ -7,10 +8,48 @@ import { NzInputModule } from 'ng-zorro-antd/input';
 
 @Component({
   selector: 'app-login',
-  imports: [FormsModule, NzFormModule, NzInputModule, NzButtonModule, NzCardModule],
+  imports: [
+    FormsModule,
+    NzFormModule,
+    NzInputModule,
+    NzButtonModule,
+    NzCardModule,
+    RecaptchaModule
+  ],
   templateUrl: './login.component.html',
-  styleUrl: './login.component.css'
+  styleUrl: './login.component.css',
 })
 export class LoginComponent {
 
+  private fb = inject(FormBuilder);
+
+  loginForm!: FormGroup;
+  recaptchaCompleted = false;
+  recaptchaToken: string | null = null;
+
+  constructor() {
+    this.loginForm = this.fb.group({
+      username: ['', Validators.required],
+      password: ['', Validators.required],
+    });
+  }
+  onCaptchaResolved(token: string | null) {
+    if (token) {
+      console.log('reCAPTCHA resuelto con token:', token);
+      this.recaptchaCompleted = true;
+      this.recaptchaToken = token;
+    } else {
+      console.log('reCAPTCHA falló o expiró');
+      this.recaptchaCompleted = false;
+      this.recaptchaToken = null;
+    }
+  }
+  onSubmit() {
+    if (this.loginForm.invalid || !this.recaptchaCompleted) {
+      console.log('Formulario inválido o captcha no resuelto');
+      return;
+    }
+
+    console.log('Formulario enviado:', this.loginForm.value, 'Token:', this.recaptchaToken);
+  }
 }
